07898c72da8aeba2d778980b852cee34ba07b5d2
mari-2110 <121809104+mari-2110@users.noreply.github.com>
2023-03-02 13:26:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const inputField = document.querySelector('.inputField');
+const btn = document.querySelector('.btn');
+const toDoContainer = document.querySelector('.toDoContainer');
+
+btn.addEventListener('click', () => {
+    const item = document.createElement('li');
+    item.innerText = inputField.value;
+    toDoContainer.appendChild(item);
+    item.classList.add('toDoAdded');
+    inputField.value = " ";
+
+    item.addEventListener('click', () => {
+        item.classList.add('toDoCompleted');
+    })
+
+    item.addEventListener('dblclick', () => {
+        toDoContainer.removeChild(item);
+    })
+})
+
+
